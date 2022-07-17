e5d417f87a16fd10fcfa399ae591cb8d76ee125c
xitras7776 <106880768+xitras7776@users.noreply.github.com>
2022-07-17 15:29:11 +0900

    ボタンの作成

---
+++ src/stories/atoms-button.stories.js
@@ -0,0 +1,6 @@
+import button from "../components/atoms/button/button.html";
+import "../components/atoms/button/button.scss";
+
+export default { title: "Atoms/button" }
+
+export const Example = () => button;
